851fc70853e506848bbfd7fda6414324d2e3b169
Tobi Alagbe <alagbet100@gmail.com>
2021-12-08 17:58:33 +0100

    graph - depth first traversal

---
+++ Graphs/traverse.js
@@ -0,0 +1,30 @@
+const graph = {
+    a: ['b', 'c'],
+    b: ['d'],
+    c: ['e'],
+    d: [],
+    e: ['b'],
+    f: ['d']
+}
+
+
+
+const traverseGraphDepthFirst = (graph, start) => {
+
+    const stack = [start];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+
+        for (let neighbor of graph[current]) {
+            stack.push(neighbor);
+        }
+    }
+
+
+}
+
+
+
+traverseGraphDepthFirst(graph, 'a');
